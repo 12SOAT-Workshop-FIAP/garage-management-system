59de082f18fc38e25643fb76c42d886f31c9c2e7
Fabrício Souza <94847330+fabriciosouzza@users.noreply.github.com>
2025-07-28 08:27:50 -0300

    git commit -m fix: add to domain method on vehicle"

--- src/modules/vehicles/infrastructure/vehicle-typeorm.repository.ts
+++ src/modules/vehicles/infrastructure/vehicle-typeorm.repository.ts
@@ -13,12 +13,18 @@ export class VehicleTypeOrmRepository implements VehicleRepository {
   ) {}
 
   async findAll(): Promise<Vehicle[]> {
-    const vehicle = this.repository.find();
-    return vehicle;
+    const vehicle = await this.repository.find();
+    const domainVehicle = vehicle.map(this.toDomain);
+    return domainVehicle;
   }
 
   async create(vehicle: Vehicle): Promise<Vehicle> {
     const createdVehicle = this.repository.create(vehicle);
-    return await this.repository.save(createdVehicle);
+    const savedVehicle = await this.repository.save(createdVehicle);
+    return this.toDomain(savedVehicle);
+  }
+
+  private toDomain(entity: VehicleEntity): Vehicle {
+    return new Vehicle(entity);
   }
 }
