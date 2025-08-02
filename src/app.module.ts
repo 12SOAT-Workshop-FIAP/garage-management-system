de701e0ca9bbaafb21e09437125bfe7fdd06c262
Isaac Araujo <isaac_maciel@hotmail.com>
2025-08-02 12:03:53 -0300

    feat: add user management features including user controller, services, and password hashing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,23 @@ import ormconfig from '../ormconfig';
 import { CustomersModule } from './modules/customers/presentation/customers.module';
 import { VehiclesModule } from './modules/vehicles/presentation/vehicles.module';
 import { WorkOrdersModule } from './modules/work-orders/presentation/work-orders.module';
-import { ServicesModule } from '@modules/services/services.module';
 import { CryptographyModule } from './modules/cryptography/presentation/cryptography.module';
+import { ServicesModule } from './modules/services/services.module';
+import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot(ormconfig),
     CustomersModule,
     VehiclesModule,
     WorkOrdersModule,
     ServicesModule,
     CryptographyModule,
+    UsersModule,
     // ... other modules
   ],
 })
